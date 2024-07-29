0d18745ba5c9c45db66d7b49bd05556d0d3cb306
JeongsuGil <lucy2468@naver.com>
2024-07-30 02:34:59 +0900

    Fix: 기부하기 버그 수정
    Before: 240730_02:28

--- src/frontend/src/pages/donation_pages/DonationFirstStep.jsx
+++ src/frontend/src/pages/donation_pages/DonationFirstStep.jsx
@@ -1,41 +1,59 @@
-import React from "react";
-import "./BeneficiaryBox.css";
-import TransparentButton from "../components/TransparentButton";
+import ColoredButton from "../../components/ColoredButton";
+import DonationStepsBar from "../../components/DonationStepsBar";
+import TagBox from "../../components/TagBox";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./DonationFirstStep.css";
 
-const BeneficiaryBox = ({ profileImage, name, tags, id }) => {
-  const onClickBeneficiaryDetailPageLink = (beneficiaryId, beneficiaryName) => {
-    const newWindow = window.open(
-      `/beneficiarydetailpage?beneficiaryId=${beneficiaryId}`,
-      "_blank"
-    );
+const DonationFirstStep = () => {
+  const categories = {
+    분류1: ["태그1", "태그2", "태그3", "태그4", "태그5", "태그6", "태그7", "태그8", "태그9", "태그10", "태그11", "태그12", "태그13", "태그14", "태그15", "태그16", "태그17", "태그18", "태그19", "태그20", "태그21", "태그22"],
+    분류2: ["12", "33", "44", "55", "66", "77", "88", "99", "10", "11"],
+  };
+  const [selectedTags, setSelectedTags] = useState(new Set());
 
-    if (newWindow) {
-      // Wait for the new window to load, then set the title
-      newWindow.onload = () => {
-        newWindow.document.title = `Do-Nate/수혜자/상세페이지${beneficiaryName}`;
-      };
-    }
+  const handleTagClick = (tagName) => {
+    setSelectedTags((prevSelectedTags) => {
+      const newSelectedTags = new Set(prevSelectedTags);
+      if (newSelectedTags.has(tagName)) {
+        newSelectedTags.delete(tagName);
+      } else {
+        newSelectedTags.add(tagName);
+      }
+      return newSelectedTags;
+    });
+  };
+
+  const navigate = useNavigate();
+
+  const onNextButtonClicked = () => {
+    navigate("/donation/second", { state: { fromFirstStep: true } });
   };
 
   return (
-    <div className="BeneficiaryBox">
-      <img className="profileImage" src={profileImage} alt={name} />
-      <div className="beneficiaryBoxText">
-        <div className="name">{name}</div>
-        <div className="tagList">
-          {tags.map((tag, index) => (
-            <div key={index} className="tagItem">
-              #{tag}
-            </div>
+    <div className="DonationFirstStep">
+      <DonationStepsBar stepNow={1} />
+      {Object.keys(categories).map((category) => (
+        <div key={category} className="categorySection">
+          <div className="categoryName">{category}</div>
+          {categories[category].map((tag) => (
+            <TagBox
+              key={tag}
+              tagName={tag}
+              isSelected={selectedTags.has(tag)}
+              onTagClick={() => handleTagClick(tag)}
+            />
           ))}
         </div>
-      </div>
-      <TransparentButton
-        text="> 상세 페이지"
-        onClick={() => onClickBeneficiaryDetailPageLink(id, name)}
+      ))}
+
+      <ColoredButton
+        text={"다음"}
+        type={"Orange"}
+        onClick={onNextButtonClicked}
       />
     </div>
   );
 };
 
-export default BeneficiaryBox;
+export default DonationFirstStep;
