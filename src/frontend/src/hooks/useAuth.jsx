5c992f20e492a404e5b77cde2705522a6f03e946
shimminseo <milly5194@naver.com>
2024-09-22 03:17:51 +0900

    Refactor : 코드 통합 & 패키지 통합 - 파일 전체
    Before : 240920_22:16
    
    Note: 주석 작성 & 패키지 구조 통합 & 백엔드 통합

--- src/frontend/src/hooks/useAuth.jsx
+++
@@ -1,7 +0,0 @@
-// src/hooks/useAuth.js
-import { useContext } from "react";
-import AuthContext from "../context/AuthContext";
-
-const useAuth = () => useContext(AuthContext);
-
-export default useAuth;
--- src/main/java/com/hikdonate/donate/domain/donor/dto/DonorSingUpValidationGroups.java
+++
@@ -1,1 +0,0 @@
-package com.hikdonate.donate.domain.donor.dto;
